f853041085dc415d836a20cdf027ebdbf02d765d
joseA224 <floquillo224@gmail.com>
2023-03-30 09:14:27 -0500

    confirmacion

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,5 @@
-let imgPrincipal = Document.getElementById("imgPrincipal");
+let imgBox   = document.getElementById(imgBox);
+let imgMini1 = document.getElementById*("imgMini1");
+let imgMini2 = document.getElementById*("imgMini2");
+let imgMini3 = document.getElementById*("imgMini3");
+let imgMini4 = document.getElementById*("imgMini4");
