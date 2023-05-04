f857eb529e131de3396877044676f8d832c666b8
joseA224 <floquillo224@gmail.com>
2023-05-04 11:45:44 -0500

    slider

---
+++ JS/slider.js
@@ -0,0 +1,7 @@
+let flechaderecha = document.getElementById (flechaderecha);
+let imgbird =document.getElementById(imgbird);
+
+flechaderecha.addEventListener("click", function(){
+        img   
+}
+)
